400a5e34370f93623a1a57e2fc5ce71b95cc0588
惊蛰 <jingzhe@jingzhe.local>
2018-08-27 15:01:48 +0800

    no message

--- Examples/SDWebImage Demo/AppDelegate.m
+++ Examples/SDWebImage Demo/AppDelegate.m
@@ -30,6 +30,7 @@
     self.navigationController = [[UINavigationController alloc] initWithRootViewController:masterViewController];
     self.window.rootViewController = self.navigationController;
     [self.window makeKeyAndVisible];
+    NSLog(@"修改了源码的第三方");
     return YES;
 }
 
